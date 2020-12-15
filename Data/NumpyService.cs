eca4c0007f953210c078ffbce1aa97ed48ba3caa
evren <evren07unal@gmail.com>
2020-12-15 20:16:56 +0300

    project files

---
+++ Data/NumpyService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Numpy;
+
+namespace numpyblzsrvr.Data
+{
+    public class NumpyService
+    {
+
+        public string GetNumbers(int num)
+        {
+            var ss = np.arange(num);
+            return ss.sum().repr.ToString();
+            
+        }
+
+    }
+}
